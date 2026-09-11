3f02eb868941901ce685a805483ad038988b4da3
Ajay <ajaytanguturi555@gmail.com>
2026-09-11 15:16:35 +0530

    added 3d arrs

--- arrays/main.c
+++ arrays/main.c
@@ -2,15 +2,20 @@
 int two_arr_scanf();
 int two_arr_declr();
 int arr_ele_acc();
+int three_arr_scanf();
+int three_arr_declr();
 #define ROW 2
 #define COLUMN 3
+#define BLOCK 2
 int main(){
     two_arr_scanf();            printf("\n----------------------------\n");
     two_arr_declr();            printf("\n----------------------------\n");
     arr_ele_acc();              printf("\n----------------------------\n");
+    three_arr_scanf();          printf("\n----------------------------\n");
+    three_arr_declr();
 }
 
-int two_arr_scanf(){                                         //this function shows how to init arr elements using scanf
+int two_arr_scanf(){                                         //this function shows how to init 2D arr elements using scanf
     int arr[ROW][COLUMN];
     printf("Enter elements of 2D array:");
     for(int i=0;i<ROW;i++){
@@ -27,7 +32,7 @@ int two_arr_scanf(){                                         //this function sho
 
 }
 
-int two_arr_declr(){                                         //this function shows how to init arr elements at time of declaration
+int two_arr_declr(){                                         //this function shows how to init 2D arr elements at time of declaration
     int arr[ROW][COLUMN]={{2,8,9},{7,6,4}};
     for(int i=0;i<ROW;i++){                             //printing arr elements
         for(int j=0;j<COLUMN;j++){
@@ -42,4 +47,39 @@ int arr_ele_acc(){
     int arr[ROW][COLUMN]={{2,8,9},{7,6,4}};
     int (*ptr)[COLUMN]=arr;
     printf("%d %d %d %d",arr[0][1],*(*(arr+1)+0),ptr[1][2],*(*(ptr+0)+2));
+}
+
+
+int three_arr_scanf(){                                         //this function shows how to init 3D arr elements using scanf
+    int arr[BLOCK][ROW][COLUMN];
+    printf("Enter elements of 3D array:");
+    for(int i=0;i<BLOCK;i++){
+        for(int j=0;j<ROW;j++){
+            for(int k=0;k<COLUMN;k++){
+                scanf("%d ",&arr[i][j][k]);
+            }
+        }
+    }
+    for(int i=0;i<BLOCK;i++){
+        for(int j=0;j<ROW;j++){
+            for(int k=0;k<COLUMN;k++){
+                printf("%d ",arr[i][j][k]);
+            }
+        }
+    }
+    printf("\n");
+
+}
+
+int three_arr_declr(){                                         //this function shows how to init 3D arr elements at time of declaration
+    int arr[BLOCK][ROW][COLUMN]={{{2,8,9},{7,6,4}},{{1,2,3},{4,5,6}}};
+    for(int i=0;i<BLOCK;i++){                             //printing arr elements
+        for(int j=0;j<ROW;j++){
+            for(int k=0;k<COLUMN;k++){
+                printf("%d ",arr[i][j][k]);
+            }
+        }
+    }
+    printf("\n");
+
 }
