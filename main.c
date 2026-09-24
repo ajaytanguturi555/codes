e223683bb47bb9c160a9751ff06a52075c3868c9
Ajay <ajaytanguturi555@gmail.com>
2026-09-24 16:01:20 +0530

    main_added

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
- #include <stdio.h>
-    void foo(int (*ary)[3]);
+    #include <stdio.h>
+
+int add(int a,int b){
+    return a+b;
+}
+int (*op)(int,int);
     int main()
     {
-        int ary[2][3];
-        foo(ary);
+        op=add;
+        (*op)(10,20);
     }
-    void foo(int (*ary)[3])
-    {
-        int i = 10, j = 2, k;
-        ary[0] = &i;
-        ary[1] = &j;
-        for (k = 0;k < 2; k++)
-        printf("%d\n", *ary[k]);
-    }
+
+   
